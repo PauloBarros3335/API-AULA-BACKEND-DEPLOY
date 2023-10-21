a680a2f54f14d51c1884fbfff0acc10a844d5613
Paulo Barros <barrosp335@gmail.com>
2023-10-21 07:52:52 -0300

    implementacao da API carros

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
+const knex = require("./conexao");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  return res.json("Api está Ok!");
+  try {
+    const carros = await knex("carros");
+    return res.json(carros);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ mensagem: "Erro do servidor" });
+  }
 });
 
 const port = process.env.PORT || 3000;
